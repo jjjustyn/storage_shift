66db0701956c2b477d2fa570d2ef37fdfdbe9cd2
jjjustyn <justynjennings@gmail.com>
2023-07-07 11:32:29 -0500

    added terms and privacy pages, changed contact form, added quote estimate popup

---
+++ estimate.js
@@ -0,0 +1,22 @@
+// Get the popup
+var popup = document.getElementById("get-estimate-popup");
+
+// Get the close button
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the popup
+window.onload = function() {
+  popup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the popup
+span.onclick = function() {
+  popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+}
